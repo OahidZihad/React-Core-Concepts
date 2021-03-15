42cfe1bacf093697eb0dea317f1643f173424001
OahidZihad <oahidzihad1@gmail.com>
2021-03-16 00:43:21 +0600

    JSX, Dynamic content, Dynamic Style in React

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 
 function App() {
+  var person = {
+    name: "Dr. Mahfuz",
+    job: 'Singer'
+  }
+  var style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +18,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Heading: {(2+3)*5-222+25555}</h1>
+        <h2 style={style}>Name: {person.name}</h2>
+        <h2 style={{backgroundColor: 'cyan', color: 'tomato'}}>Occupation: {person.job}</h2>
+        <p>My first React Paragraph</p>
       </header>
     </div>
   );
